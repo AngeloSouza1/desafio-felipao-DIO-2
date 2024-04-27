a8161d2d2d8a5bb9bb1de2f013115df679dd1b03
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-04-27 12:26:33 -0300

    feat: inclusao da mensagem central e ajuste tabela resultado

--- Desafio.rb
+++ Desafio.rb
@@ -6,13 +6,19 @@ def clear_screen
 end
 
 
-def botoes(x, y)
-  button_width = 20 # Largura
-
-  # Exibir botão na posição especificada
-  puts "\e[#{y};#{x}H" + "Testar Função".center(button_width).colorize(background: :blue)
-
+def mensagem_superior(screen_width)
+  mensagem_superior = "   Essa aplicação tem como objetivo, testar a função".colorize(:yellow)
+  mensagem_superior1 = "                  CALCULAR_RANKEADAS".colorize(:cyan)
+  mensagem_superior2 = "     Projeto proposto pela DIO - Bootcamp Santander".colorize(:yellow)
+  mensagem_superior3 = "           Para testar, aperte a tecla ENTER".colorize(:green)
+  largura_mensagem = mensagem_superior.length  # Obtém a largura da mensagem
+  x_superior = (screen_width - largura_mensagem) / 2  # Calcula a posição horizontal para centralizar a mensagem
+  puts "\e[12;#{x_superior}H" + mensagem_superior
+  puts "\e[14;#{x_superior}H" + mensagem_superior1
+  puts "\e[16;#{x_superior}H" + mensagem_superior2
+  puts "\e[20;#{x_superior}H" + mensagem_superior3
 end
+
 
 def print_header(screen_width)
   header = "Desafio da DIO 2 - Calculadora de partidas Rankeadas"
@@ -23,12 +29,12 @@ end
 
 def print_table(heroes)
 puts "+" + "-" * 50 + "+"
-puts "| #{'Herói'.ljust(20)} | #{'Vitórias'.center(10)} | #{'Derrotas'.center(10)} |"
+puts "| #{'Herói'.ljust(20)} | #{'Vitórias'.center(10)} | #{'Derrotas'.center(10)} |".colorize(background: :blue)
 puts "+" + "-" * 50 + "+"
 heroes.each do |hero|
   nome_justificado = hero[:nome].ljust(20)
-  vitorias_justificado = hero[:vitorias].to_s.center(10)
-  derrotas_justificado = hero[:derrotas].to_s.center(10)
+  vitorias_justificado = hero[:vitorias].to_s.center(10).colorize(:cyan)
+  derrotas_justificado = hero[:derrotas].to_s.center(10).colorize(:red)
   puts "| #{nome_justificado} | #{vitorias_justificado} | #{derrotas_justificado} |"
 end
 puts "+" + "-" * 50 + "+"
@@ -38,11 +44,19 @@ end
 def print_table_right(screen_width)
   table_width = 50  # Largura da tabela
   x_position = screen_width - table_width - 1  # Posição inicial da tabela
-  puts "\n"
-  puts "\e[3;#{x_position}H+" + "-" * (table_width - 2) + "+"
-  puts "\e[4;#{x_position}H| #{'Herói'.ljust(20)} | #{'Vitórias'.center(10)} | #{'Nível'.center(10)} |"
-  puts "\e[5;#{x_position}H+" + "-" * (table_width - 2) + "+"
+
+  # Linha superior da tabela
+  puts "\e[4;#{x_position}H+" + "-" * (table_width - 2) + "+"
+
+  # Cabeçalho da tabela com fundo azul
+  cabecalho = "| #{'Herói'.ljust(20)} | #{'Vitórias'.center(10)} | #{'Nível'.center(10)} |"
+  puts "\e[5;#{x_position}H#{cabecalho.colorize(background: :blue)}"
+
+  # Linha intermediária da tabela
   puts "\e[6;#{x_position}H+" + "-" * (table_width - 2) + "+"
+
+  # Linha inferior da tabela
+  puts "\e[7;#{x_position}H+" + "-" * (table_width - 2) + "+"
 end
 
 
@@ -80,10 +94,9 @@ heroes = [
   { nome: "Ciborgue", vitorias: 12, derrotas: 12 },
   { nome: "Arqueiro Verde", vitorias: 30, derrotas: 25 },
   { nome: "Shazam", vitorias: 60, derrotas: 20 },
-  { nome: "Cyborg Superman", vitorias: 100, derrotas: 0 },
+  { nome: "Cyborg Superman", vitorias: 100, derrotas: 1 },
   { nome: "Supergirl", vitorias: 35, derrotas: 15 },
   { nome: "Robin", vitorias: 20, derrotas: 18 },
-  { nome: "Aqualad", vitorias: 8, derrotas: 5 },
   { nome: "Donna Troy", vitorias: 50, derrotas: 10 },
   { nome: "Martian Manhunter", vitorias: 85, derrotas: 7 },
   { nome: "Zatanna", vitorias: 30, derrotas: 22 },
@@ -101,12 +114,9 @@ heroes = [
   { nome: "Blue Beetle", vitorias: 35, derrotas: 25 },
   { nome: "Booster Gold", vitorias: 60, derrotas: 30 },
   { nome: "Starfire", vitorias: 75, derrotas: 20 },
+  { nome: "Chapolin", vitorias: 110, derrotas: 0 },
   { nome: "Beast Boy", vitorias: 45, derrotas: 25 }
 ]
-
-
-
-
 
 clear_screen
 # Obtém a largura da tela
@@ -115,14 +125,12 @@ screen_width = `tput cols`.to_i
 # Imprime o cabeçalho
 print_header(screen_width)
 
-# Chamada da função para exibir o botão
-botoes(70,4)
-
-
 # Imprime a tabela de heróis
  print_table(heroes)
-
  print_table_right(screen_width)
+
+mensagem_superior(screen_width)
+
 
 gets
 clear_screen
