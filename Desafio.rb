4fd0f4193c7de0d328865e80edf271252724111f
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-04-27 16:08:06 -0300

    feat: inclusão funcao imagem trofeu

--- Desafio.rb
+++ Desafio.rb
@@ -5,18 +5,46 @@ def clear_screen
   system "clear" or system "cls"
 end
 
+def imagem_trofeu(x, y, deslocamento_x)
+  # Define o texto do troféu
+  trofeu = <<-'ASCII'
+     ___________  
+    '._==_==_=_.' 
+    .-\:      /-. 
+   | (|:.     |) |
+    '-|:.     |-' 
+      \::.    /   
+       '::. .'    
+         ) (      
+       _.' '._    
+      `"""""""`  
+  ASCII
+
+  # Divide o texto do troféu em linhas e adiciona o deslocamento no eixo x
+  trofeu.split("\n").each_with_index do |line, index|
+    # Imprime cada linha com o deslocamento no eixo x
+    puts "\e[#{y + index};#{x + deslocamento_x}H#{line.colorize(:light_cyan)}"
+  end
+end
+
+
+
+
+
 
 def mensagem_superior(screen_width)
-  mensagem_superior = "   Essa aplicação tem como objetivo, testar a função".colorize(:yellow)
-  mensagem_superior1 = "                  CALCULAR_RANKEADAS".colorize(:cyan)
-  mensagem_superior2 = "     Projeto proposto pela DIO - Bootcamp Santander".colorize(:yellow)
-  mensagem_superior3 = "           Para testar, aperte a tecla ENTER".colorize(:green)
+  mensagem_superior = " Essa aplicação tem como objetivo,".colorize(:yellow)
+  mensagem_superior1= " testar a função".colorize(:yellow) + " CALCULAR_RANKEADAS".colorize(:cyan)
+  mensagem_superior2 = " Projeto proposto pela DIO".colorize(:yellow)
+  mensagem_superior3 = " --- Bootcamp Santander ---".colorize( color: :light_red)
+  mensagem_superior4 = " Para testar, aperte a tecla ENTER".colorize(:green)
   largura_mensagem = mensagem_superior.length  # Obtém a largura da mensagem
-  x_superior = (screen_width - largura_mensagem) / 2  # Calcula a posição horizontal para centralizar a mensagem
-  puts "\e[12;#{x_superior}H" + mensagem_superior
-  puts "\e[14;#{x_superior}H" + mensagem_superior1
-  puts "\e[16;#{x_superior}H" + mensagem_superior2
-  puts "\e[20;#{x_superior}H" + mensagem_superior3
+  x_superior = (screen_width - largura_mensagem)-138/ 2  # Calcula a posição horizontal para centralizar a mensagem
+  puts "\e[20;#{x_superior}H" + mensagem_superior
+  puts "\e[22;#{x_superior}H" + mensagem_superior1
+  puts "\e[24;#{x_superior}H" + mensagem_superior2
+  puts "\e[26;#{x_superior}H" + mensagem_superior3
+  puts "\e[30;#{x_superior}H" + mensagem_superior4
 end
 
 
@@ -34,7 +62,7 @@ puts "+" + "-" * 50 + "+"
 heroes.each do |hero|
   nome_justificado = hero[:nome].ljust(20)
   vitorias_justificado = hero[:vitorias].to_s.center(10).colorize(:cyan)
-  derrotas_justificado = hero[:derrotas].to_s.center(10).colorize(:red)
+  derrotas_justificado = hero[:derrotas].to_s.center(10).colorize(:light_red)
   puts "| #{nome_justificado} | #{vitorias_justificado} | #{derrotas_justificado} |"
 end
 puts "+" + "-" * 50 + "+"
@@ -42,14 +70,14 @@ puts "+" + "-" * 50 + "+"
 end
 
 def print_table_right(screen_width)
-  table_width = 50  # Largura da tabela
+  table_width = 80  # Largura da tabela
   x_position = screen_width - table_width - 1  # Posição inicial da tabela
 
   # Linha superior da tabela
   puts "\e[4;#{x_position}H+" + "-" * (table_width - 2) + "+"
 
   # Cabeçalho da tabela com fundo azul
-  cabecalho = "| #{'Herói'.ljust(20)} | #{'Vitórias'.center(10)} | #{'Nível'.center(10)} |"
+  cabecalho = "| #{'Herói'.ljust(20)} | #{'Status'.center(52)}  |"
   puts "\e[5;#{x_position}H#{cabecalho.colorize(background: :blue)}"
 
   # Linha intermediária da tabela
@@ -125,9 +153,13 @@ screen_width = `tput cols`.to_i
 # Imprime o cabeçalho
 print_header(screen_width)
 
+
 # Imprime a tabela de heróis
  print_table(heroes)
+
+imagem_trofeu(10, 5, 48)
  print_table_right(screen_width)
+
 
 mensagem_superior(screen_width)
 
