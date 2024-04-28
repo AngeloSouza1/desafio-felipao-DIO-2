72781daf7af5d03c873420205b498756cecbcdb7
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-04-28 10:24:25 -0300

    chore: ajuste dos links para utilizacao no programa

--- Desafio.rb
+++ Desafio.rb
@@ -27,17 +27,13 @@ def imagem_trofeu(x, y, deslocamento_x)
   end
 end
 
-
-
-
-
-
 def mensagem_superior(screen_width)
   mensagem_superior = " Essa aplicação tem como objetivo,".colorize(:yellow)
   mensagem_superior1= " testar a função".colorize(:yellow) + " CALCULAR_RANKEADAS".colorize(:cyan)
   mensagem_superior2 = "    Projeto proposto pela  D I O".colorize(:yellow)
   mensagem_superior3 = "    --- Bootcamp Santander ---".colorize( color: :light_red)
-  mensagem_superior4 = " Para testar, aperte a tecla ENTER".colorize(:green)
+  mensagem_superior4 = "   Para testar, aperte uma 'tecla'".colorize(:green)
+  mensagem_superior5 = "   Para sair,   aperte a tecla 's'".colorize(:green)
   largura_mensagem = mensagem_superior.length  # Obtém a largura da mensagem
   x_superior = (screen_width - largura_mensagem)-138/ 2  # Calcula a posição horizontal para centralizar a mensagem
   puts "\e[16;#{x_superior}H" + mensagem_superior
@@ -45,8 +41,8 @@ def mensagem_superior(screen_width)
   puts "\e[20;#{x_superior}H" + mensagem_superior2
   puts "\e[22;#{x_superior}H" + mensagem_superior3
   puts "\e[30;#{x_superior}H" + mensagem_superior4
+  puts "\e[31;#{x_superior}H" + mensagem_superior5
 end
-
 
 def print_header(screen_width)
   header = "Desafio da DIO 2 - Calculadora de partidas Rankeadas"
@@ -56,17 +52,16 @@ def print_header(screen_width)
 end
 
 def print_table(heroes)
-puts "+" + "-" * 50 + "+"
-puts "| #{'Herói'.ljust(20)} | #{'Vitórias'.center(10)} | #{'Derrotas'.center(10)} |".colorize(background: :blue)
-puts "+" + "-" * 50 + "+"
-heroes.each do |hero|
-  nome_justificado = hero[:nome].ljust(20)
-  vitorias_justificado = hero[:vitorias].to_s.center(10).colorize(:cyan)
-  derrotas_justificado = hero[:derrotas].to_s.center(10).colorize(:light_red)
-  puts "| #{nome_justificado} | #{vitorias_justificado} | #{derrotas_justificado} |"
-end
-puts "+" + "-" * 50 + "+"
-
+  puts "+" + "-" * 50 + "+"
+  puts "| #{'Herói'.ljust(20)} | #{'Vitórias'.center(10)} | #{'Derrotas'.center(10)} |".colorize(background: :blue)
+  puts "+" + "-" * 50 + "+"
+  heroes.each do |hero|
+    nome_justificado = hero[:nome].ljust(20)
+    vitorias_justificado = hero[:vitorias].to_s.center(10).colorize(:cyan)
+    derrotas_justificado = hero[:derrotas].to_s.center(10).colorize(:light_red)
+    puts "| #{nome_justificado} | #{vitorias_justificado} | #{derrotas_justificado} |"
+  end
+  puts "+" + "-" * 50 + "+"
 end
 
 def print_table_right(screen_width, heroes)
@@ -77,7 +72,7 @@ def print_table_right(screen_width, heroes)
   puts "\e[4;#{x_position}H+" + "-" * (table_width - 2) + "+"
 
   # Cabeçalho da tabela com fundo azul
-  cabecalho = "| #{'Herói'.ljust(20)} | #{'Status'.center(52)}  |"
+  cabecalho = "| #{'Herói'.ljust(17)} | #{'Status de Vitórias'.center(55)} |"
   puts "\e[5;#{x_position}H#{cabecalho.colorize(background: :blue)}"
 
   # Linha intermediária da tabela
@@ -103,30 +98,17 @@ def print_table_right(screen_width, heroes)
     status_cell = status.ljust(remaining_length)
 
     # Imprime a linha da tabela
-    puts "\e[#{index + 7};#{x_position}H| #{nome} | #{status_cell}     |"
+    puts "\e[#{index + 7};#{x_position}H| #{nome} | #{status_cell}    |"
   end
 
   # Linha inferior da tabela
   puts "\e[#{heroes_sorted.length + 7};#{x_position}H+" + "-" * (table_width - 2) + "+"
 end
 
-
-
 def calcular_rankeadas(vitorias, derrotas)
   saldo_vitorias = vitorias - derrotas
 
-            def print_table_right(screen_width, heroes)
-              table_width = 80  # Largura da tabela
-              x_position = screen_width - table_width - 1  # Posição inicial da tabela
-
-              # Linha superior da tabela
-              puts "\e[4;#{x_position}H+" + "-" * (table_width - 2) + "+"
-
-
-              # Linha inferior da tabela
-              puts "\e[#{heroes.length + 7};#{x_position}H+" + "-" * (table_width - 2) + "+"
-            end
-          nivel = if saldo_vitorias < 10
+  nivel = if saldo_vitorias < 10
             "O Herói tem de saldo de  #{saldo_vitorias} está no nível de Ferro".colorize(:light_green)
           elsif saldo_vitorias.between?(11, 20)
             "O Herói tem de saldo de #{saldo_vitorias} está no nível de Bronze".colorize(:light_yellow)
@@ -145,58 +127,63 @@ def calcular_rankeadas(vitorias, derrotas)
   return saldo_vitorias, nivel
 end
 
-# Lista de heróis
-heroes = [
-  { nome: "Batman", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Superman", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Mulher Maravilha", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Flash", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Aquaman", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Lanterna Verde", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Ciborgue", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Arqueiro Verde", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Shazam", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Cyborg Superman", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Supergirl", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Robin", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Donna Troy", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Martian Manhunter", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Zatanna", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Constantine", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Hawkman", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Hawkgirl", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Black Canary", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Green Arrow", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Wonder Twins", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Apache Chief", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Elongated Man", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Plastic Man", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Red Tornado", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Huntress", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Blue Beetle", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Booster Gold", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Starfire", vitorias: rand(1..150), derrotas: rand(1..30) },
-  { nome: "Chapolin", vitorias: 150, derrotas: 0 },
-  { nome: "Beast Boy", vitorias: rand(1..150), derrotas: rand(1..30) }
-]
-
-clear_screen
 # Obtém a largura da tela
 screen_width = `tput cols`.to_i
 
-# Imprime o cabeçalho
-print_header(screen_width)
+loop do
+  clear_screen
+    # Imprime o cabeçalho
+  print_header(screen_width)
 
+  # Lista de heróis
+  heroes = [
+    { nome: "Batman", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Superman", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Mulher Maravilha", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Flash", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Aquaman", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Lanterna Verde", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Ciborgue", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Arqueiro Verde", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Shazam", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Cyborg Superman", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Supergirl", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Robin", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Donna Troy", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Martian Manhunter", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Zatanna", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Constantine", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Hawkman", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Hawkgirl", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Black Canary", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Green Arrow", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Wonder Twins", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Apache Chief", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Elongated Man", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Plastic Man", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Red Tornado", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Huntress", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Blue Beetle", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Booster Gold", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Starfire", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Chapolin", vitorias: rand(1..150), derrotas: rand(1..30) },
+    { nome: "Beast Boy", vitorias: rand(1..150), derrotas: rand(1..30) }
+  ]
 
-# Imprime a tabela de heróis
- print_table(heroes)
+  # Imprime a tabela de heróis
+  print_table(heroes)
 
- imagem_trofeu(10, 5, 48)
+  imagem_trofeu(10, 5, 48)
 
+  # Imprime a tabela de heróis à direita
   print_table_right(screen_width, heroes)
 
-mensagem_superior(screen_width)
+  # Imprime a mensagem superior
+  mensagem_superior(screen_width)
 
+  # Aguarda a entrada do usuário para atualizar os dados
+  input = STDIN.getch
+  break if  input == 's'
 
-gets
+end
 clear_screen
